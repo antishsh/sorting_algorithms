d9d93f4233c7bbdf235ee56cfc6e04e8de39bb1b
Anteneh Shimelis <48884352+antishsh@users.noreply.github.com>
2022-08-15 14:48:25 +0300

    chanmge

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,95 +1,84 @@
 #include "sort.h"
 
-void divide(int beg, int pivot, int *i, size_t size);
-int partition(int beg, int pivot, int *i, size_t size);
-void swap_int(int *a, int *b);
 /**
- * quick_sort - sorts an array of integers in ascending order
- * @array: array to be sorted
- * @size: size of the array
+ * _swap - swaps two numbers.
+ * @a: integer
+ * @b: integer
+ */
+void _swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * _split - splits the array and takes the last element as pivot
+ * @arr: input array
+ * @min: first element
+ * @last: The last element
+ * @size: size
+ * Return: integer
+ */
+int _split(int *arr, int min, int last, size_t size)
+{
+	int piv;
+	int i = (min);
+	int j;
+
+	piv = arr[last];
+	for (j = min; j < last; j++)
+	{
+		if (arr[j] <= piv)
+		{
+			_swap(&arr[i], &arr[j]);
+
+			if (i != j)
+				print_array(arr, size);
+
+			i++;
+		}
+	}
+
+	_swap(&arr[i], &arr[last]);
+	if (i != j)
+	{
+		print_array(arr, size);
+	}
+	return (i);
+}
+
+/**
+ * quick_sort_array - quick_sort_array
+ * @arr: arr
+ * @min: min
+ * @last: last
+ * @size: size
+ */
+void quick_sort_array(int *arr, int min, int last, size_t size)
+{
+	int piv;
+
+	if (min < last)
+	{
+		piv = _split(arr, min, last, size);
+		quick_sort_array(arr, min, (piv - 1), size);
+		quick_sort_array(arr, (piv + 1), last, size);
+	}
+}
+
+/**
+ * quick_sort -Sort an array using quick_sort algorithm
+ * @array: array
+ * @size: size
  */
 void quick_sort(int *array, size_t size)
 {
-	int beg = 0, pivot;
-
-	if (array && size > 1)
+	if (size < 2)
 	{
-		pivot = (size - 1);
-		divide(beg, pivot, array, size);
+		return;
 	}
-}
-/**
- * divide - recursively partition
- * @beg: beginning of divided array
- * @pivot: end of divided array
- * @i: the beginning of the array
- * @size: size of array
- */
-void divide(int beg, int pivot, int *i, size_t size)
-{
-	int first, second, np;
-
-	if (beg < pivot)
-	{
-		second = partition(beg, pivot, i, size);
-		first = beg;
-		np = second - 1;
-		if (first != np && second != pivot)
-			np--;
-		divide(first, np, i, size);
-		divide(second, pivot, i, size);
-	}
-}
-/**
- * partition - divides an array
- * @beg: beginning of array separated
- * @pivot: end of array separated
- * @i: the beginning of array
- * @size: size of array
- * Return: the new beginning
- */
-int partition(int beg, int pivot, int *i, size_t size)
-{
-	int temp;
-
-	temp = beg;
-	while (temp != pivot)
-	{
-		if (i[temp] < i[pivot])
-		{
-			if (temp != beg)
-			{
-				swap_int(i + temp, i + beg);
-				print_array(i, size);
-			}
-			temp++;
-			beg++;
-		}
-		else
-			temp++;
-	}
-	if (beg != pivot)
-	{
-		if (i[beg] > i[pivot])
-		{
-			swap_int(i + pivot, i + beg);
-			print_array(i, size);
-		}
-		beg++;
-	}
-	return (beg);
-}
-
-/**
- * swap_int - swaps the values of two integers
- * @a: take an int
- * @b: take an int
- */
-void swap_int(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
+	quick_sort_array(array, 0, size - 1, size);
 }
