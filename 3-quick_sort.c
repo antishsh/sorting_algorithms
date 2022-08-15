29302761eab315127e0284d98ce5f7b7b18f6704
Anteneh Shimelis <48884352+antishsh@users.noreply.github.com>
2022-08-15 11:36:25 +0300

    correction

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -20,7 +20,7 @@ void quick_sort(int *array, size_t size)
  */
 void recursive_quick_sort(int *array, size_t size, int start, int end)
 {
-	int p;
+	int p; 
 	if (start < end)
 	{
 		p = partition(array, size, start, end);
@@ -67,7 +67,7 @@ size_t partition(int *array, size_t size, int start, int end)
 void swap_int1(int *array, int a, int b)
 {
 	int tmp;
-	tmp = array[a];
+	tmp = array[a]; 
 	array[a] = array[b];
 	array[b] = tmp;
 }
